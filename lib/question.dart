9118628813fc98b91a0b2d0facb32239fc36924b
Dhiraj320 <dhirajkumawat320@gmail.com>
2022-05-01 22:18:56 +0530

    added quizzle_r file added

---
+++ lib/question.dart
@@ -0,0 +1,18 @@
+// class Question {
+//   String questionText;
+//   bool questionAnswer;
+//
+//   Question({ String q,  bool a}){
+//
+//     questionText = q,
+//     questionAnswer = a;
+//
+//   }
+// }
+
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question({required this.questionText, required this.questionAnswer});
+}
